4b98e82e40b43eb755698d2ff460af334821d23b
kirannambiar12 <kiran.nambiar@atriarenewable.com>
2026-02-11 20:04:33 +0530

    updated the appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: process.env.SANITY_STUDIO_DATASET || 'development',
   },
   deployment: {
+    appId: process.env.SANITY_STUDIO_APP_ID || '',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
